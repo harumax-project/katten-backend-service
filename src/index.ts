e0e7b9486782772d8af23e30693991fef72a0cec
maxkenkomoda <tango12speedcell@gmail.com>
2021-11-30 22:15:08 +0900

    implement cors handle options

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import { CORS_OPTIONS } from './common/cors-option'
+
+const app = express()
+app.use(express.json())
+app.use(cors(CORS_OPTIONS))
+
+app.route('/').get((req, res) => {
+  res.send('welcome to katten backend service!')
+})
+
+
+const port = process.env.PORT || 3100
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
